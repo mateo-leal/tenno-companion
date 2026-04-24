fed68064e9683b15bfefd3175ce71b3b5affa385
Mateo Leal <mateolegi@gmail.com>
2026-04-24 03:08:19 -0500

    add release version to sentry configuration

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -3,10 +3,10 @@
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://96d68988e9c777b85d268bdd02a02eef@o4511262408900608.ingest.us.sentry.io/4511262410801152",
+  dsn: 'https://96d68988e9c777b85d268bdd02a02eef@o4511262408900608.ingest.us.sentry.io/4511262410801152',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -14,7 +14,9 @@ Sentry.init({
   // Enable logs to be sent to Sentry
   enableLogs: true,
 
+  release: 'web@' + process.env.version,
+
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-});
+})
--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -2,10 +2,10 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://96d68988e9c777b85d268bdd02a02eef@o4511262408900608.ingest.us.sentry.io/4511262410801152",
+  dsn: 'https://96d68988e9c777b85d268bdd02a02eef@o4511262408900608.ingest.us.sentry.io/4511262410801152',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -13,7 +13,9 @@ Sentry.init({
   // Enable logs to be sent to Sentry
   enableLogs: true,
 
+  release: 'web@' + process.env.version,
+
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-});
+})
